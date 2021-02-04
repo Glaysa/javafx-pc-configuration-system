ee78fc3258b71a10101f9b76410621bb695dcf6f
Glaysa <glaysa.df@gmail.com>
2021-02-04 23:28:44 +0100

    updated gui and added data collection

--- src/main/java/org/openjfx/controllers/ControllerAdmin.java
+++ src/main/java/org/openjfx/controllers/ControllerAdmin.java
@@ -1,29 +1,41 @@
 package org.openjfx.controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import org.openjfx.data_collection.ComponentsCollection;
 import org.openjfx.data_models.PCComponents;
 import org.openjfx.gui_utilities.Dialogs;
 
-public class ControllerAdmin {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class ControllerAdmin implements Initializable {
+
+    @FXML private TableView<PCComponents> tableView;
+    @FXML private TextField searchInput;
     @FXML private TextField cName;
-    @FXML private TextArea cDesc;
     @FXML private TextField price;
+    @FXML private TextArea cDesc;
     @FXML private ComboBox<?> cType;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ComponentsCollection.setTableView(tableView);
+    }
+
     @FXML
-    void addComponent(ActionEvent event) {
+    void addComponent() {
         String componentName = cName.getText();
         String componentDesc = cDesc.getText();
         String componentPrice = price.getText();
 
         try {
             PCComponents c = new PCComponents(componentName,componentDesc,"type",componentPrice);
-            System.out.println(c.toString());
+            ComponentsCollection.addToCollection(c);
             resetFields();
 
             Dialogs.showSuccessDialog("Component Added Successfully!");
@@ -32,10 +44,15 @@ public class ControllerAdmin {
         }
     }
 
+    @FXML
+    void search(){
+        throw new UnsupportedOperationException("Method not yet implemented");
+    }
+
     void resetFields(){
         cName.setText("");
         cDesc.setText("");
         price.setText("");
-        System.out.println("A new component has been added!");
+        tableView.refresh();
     }
 }
---
+++ src/main/java/org/openjfx/data_collection/ComponentsCollection.java
@@ -0,0 +1,29 @@
+package org.openjfx.data_collection;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableView;
+import org.openjfx.data_models.PCComponents;
+
+import java.util.ArrayList;
+
+public class ComponentsCollection {
+
+    private static ObservableList<PCComponents> obsList = FXCollections.observableArrayList();
+
+    public static void addToCollection(PCComponents toAdd){
+        obsList.add(toAdd);
+    }
+
+    public static void setTableView(TableView<PCComponents> tableView){
+        tableView.setItems(obsList);
+    }
+
+    public static void setObsList(ArrayList<PCComponents> data) {
+        ComponentsCollection.obsList = FXCollections.observableArrayList(data);
+    }
+
+    public static ObservableList<PCComponents> getObsList() {
+        return obsList;
+    }
+}
