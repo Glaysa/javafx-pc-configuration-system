85c2fe5bc3f0644728dac993b38a2f612e8ae153
Glaysa <glaysa.df@gmail.com>
2021-04-22 21:16:35 +0200

    added gson dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,11 @@
 module org.openjfx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
     opens org.openjfx to javafx.fxml;
-    opens org.openjfx.controllers to javafx.fxml;
-    opens org.openjfx.dataModels to javafx.base;
+    opens org.openjfx.controllers to javafx.fxml, com.google.gson;
+    opens org.openjfx.dataModels to javafx.base, com.google.gson;
 
     exports org.openjfx;
 }
