169f69e151bb13e210f3c9a6db0321da597c63f9
Glaysa <glaysa.df@gmail.com>
2021-04-24 14:55:19 +0200

    created customer fxml and controller

--- src/main/java/org/openjfx/controllers/ControllerCustomer.java
+++ src/main/java/org/openjfx/controllers/ControllerCustomer.java
@@ -1,5 +1,63 @@
 package org.openjfx.controllers;
 
-public class ControllerCustomer {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import org.openjfx.dataModels.PCComponents;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class ControllerCustomer implements Initializable {
+
+    @FXML private Label totalPriceLabel;
+    @FXML private TextField searchInput;
+    @FXML private TableView<PCComponents> tableViewCart;
+    @FXML private TableView<PCComponents> tableViewProducts;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    void addToCart() {
+
+    }
+
+    @FXML
+    void checkout() {
+
+    }
+
+    @FXML
+    void filterTableView() {
+
+    }
+
+    @FXML
+    void logout() {
+
+    }
+
+    @FXML
+    void openFile() {
+
+    }
+
+    @FXML
+    void removeFromCart() {
+
+    }
+
+    @FXML
+    void saveChanges() {
+
+    }
+
+    @FXML
+    void saveFile() {
+
+    }
 }
